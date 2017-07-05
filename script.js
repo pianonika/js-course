918233a6639798c2ba2b7a7766eb5be3d90e9581
Natalyremote <nate.morizo@gmail.ru>
2017-07-05 20:06:40 +0400

    fix_VKpromise

--- script.js
+++ script.js
@@ -49,13 +49,9 @@ new Promise(resolve => window.onload = resolve)
     // })
     .then(() => vkApi('friends.get', {fields: 'photo_200'}))
     .then(response => friends.innerHTML = templateFn(response))
-    .then(init())
+    .then(() => init())
     .catch(e => alert('Ошибка: ' + e.message));
 
-function handleDragStart(e) {
-    this.style.opacity = '0.4';
-    console.log('handleDragStart');
-}
 
 function handleDragOver(e) {
     if (e.preventDefault) {
@@ -67,29 +63,8 @@ function handleDragOver(e) {
     return false;
 }
 
-// function handleDragEnter(e) {
-//     // this / e.target is the current hover target.
-//     this.classList.remove('remove');
-// }
-//
-// function handleDragLeave(e) {
-//     this.classList.add('remove');  // this / e.target is previous target element.
-// }
-
-// var friendsList = document.querySelectorAll('#friends .friend');
-//
-// // [].forEach.call(friendsList, function(friend) {
-// //     friend.addEventListener('dragstart', handleDragStart, false);
-// //     // friend.addEventListener('dragenter', handleDragEnter, false)
-// //     // friend.addEventListener('dragover', handleDragOver, false);
-// //     // friend.addEventListener('dragleave', handleDragLeave, false);
-// //     // friend.addEventListener('drop', handleDrop, false);
-// //     // friend.addEventListener('dragend', handleDragEnd, false);
-// // });
-
-
 function init() {
-    var friendsList = document.querySelectorAll('#friends .friend');
+    var friendsList = document.querySelector('#friends').querySelectorAll('.friend');
 
     [].forEach.call(friendsList, function(friend) {
         friend.addEventListener('dragstart', handleDragStart, false);
